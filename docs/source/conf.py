678ca4184f1c5d23d26ce79cb9455fbdb0442f60
khrapovs <khrapovs@gmail.com>
2015-02-11 12:29:41 +0300

    Update config files to accomodate numdifftolls

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ import mock
 
 MOCK_MODULES = ['numpy', 'seaborn', 'matplotlib', 'matplotlib.pylab',
                 'statsmodels', 'statsmodels.tsa', 'statsmodels.tsa.tsatools',
-                'scipy', 'scipy.stats', 'scipy.optimize', 'scipy.linalg']
+                'scipy', 'scipy.stats', 'scipy.optimize', 'scipy.linalg',
+                'numdifftools']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
