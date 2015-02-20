537b7c9b5f104f0fbf21710558d62631503f3e73
Stanislav Khrapov <khrapovs@gmail.com>
2015-02-20 14:48:01 -0500

    Relative imports

--- argamma/arg.py
+++ argamma/arg.py
@@ -35,8 +35,8 @@ import numdifftools as nd
 from statsmodels.tsa.tsatools import lagmat
 
 from .argparams import ARGparams
-from ..mygmm import GMM
-from ..fangoosterlee import cosmethod
+from argamma.mygmm import GMM
+from argamma.fangoosterlee import cosmethod
 
 
 __author__ = "Stanislav Khrapov"
